ddeed611cad123ad33a547e5229a25a39bb95e56
bleudy TETE <bleudykitondo.bk@gmail.com>
2023-01-24 17:36:01 +0100

    feat: create admin controler

---
+++ Boite/controlers/admin.js
@@ -0,0 +1,28 @@
+import admin from '../models/admin'
+import bcrypt from 'bcrypt'
+
+export const addAdminOrFind = async (request, response) => {
+  await admin
+    .findOne({ email: request.body.email })
+    .then(user => {
+      if (user) {
+        response.status(200).json(`admin is already created`)
+      } else {
+        const hash = bcrypt.hashSync(request.body.password, 10)
+        try {
+          const newAdmin = admin.create({
+            email: request.body.email,
+            password: hash,
+          })
+          if (newAdmin) {
+            response.status(200).json({ message: 'the admin is succesfully created' })
+          } else {
+            response.status(400).json({ message: 'the admin is not created' })
+          }
+        } catch (err) {
+          throw err
+        }
+      }
+    })
+    .catch()
+}
