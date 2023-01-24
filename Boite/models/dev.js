3f3b378a65cbb896d7f28b75eaaa1a6d90b2d073
bleudy TETE <bleudykitondo.bk@gmail.com>
2023-01-24 14:17:01 +0100

    feat: add dev  mongoose modal user

---
+++ Boite/models/dev.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose'
+
+const Dev = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  sexe: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  entreprise: {
+    type: String,
+    required: true,
+  },
+  contact: {
+    type: Number,
+    required: true,
+    max: 10,
+  },
+  profession: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+})
+export default mongoose.model('dev', Dev) || mongoose.model.dev
